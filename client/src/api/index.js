53a0875473d5085c5a1b68dad2e90a5acdccb505
Daniëlle Terras <danielle.terras@student.howest.be>
2019-05-27 13:13:21 +0200

    Update package.json en update api/index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,34 +1,34 @@
 class Api {
-	constructor(entity) {
-		this.entity = entity;
-	}
+  constructor(entity) {
+    this.entity = entity;
+  }
 
-	getAll = async () => {
-		const r = await fetch(`/api/${this.entity}`);
-		// const r = await fetch(`/api/${this.entity}`);
-		return await r.json();
-	};
+  getAll = async () => {
+    const r = await fetch(`/${this.entity}`);
+    // const r = await fetch(`/api/${this.entity}`);
+    return await r.json();
+  };
 
-	create = async story => {
-		const r = await fetch(
-			`/api/${this.entity}`,
-			this.getOptions("post", story.values)
-		);
-		return await r.json();
-	};
+  create = async story => {
+    const r = await fetch(
+      `/${this.entity}`,
+      this.getOptions("post", story.values)
+    );
+    return await r.json();
+  };
 
-	getOptions = (method, body = null) => {
-		const options = {
-			method: method.toUpperCase(),
-			headers: {
-				"content-type": `application/json`
-			}
-		};
-		if (body) {
-			options.body = JSON.stringify(body);
-		}
-		return options;
-	};
+  getOptions = (method, body = null) => {
+    const options = {
+      method: method.toUpperCase(),
+      headers: {
+        "content-type": `application/json`
+      }
+    };
+    if (body) {
+      options.body = JSON.stringify(body);
+    }
+    return options;
+  };
 }
 
 export default Api;
