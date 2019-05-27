7c063ff5920f7b7fd2dd8e27bc968f259ddb563e
Daniëlle Terras <danielle.terras@student.howest.be>
2019-05-27 12:45:50 +0200

    Update index.js met enforceActions

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,31 +2,33 @@ import { decorate, observable, action, runInAction } from "mobx";
 import Story from "../models/Story";
 import Api from "../api";
 
+configure({ enforceActions: `observed` });
+
 class Store {
-	stories = [];
+  stories = [];
 
-	constructor() {
-		this.api = new Api(`stories`);
-		this.api.getAll().then(d => d.forEach(this._addStory));
-	}
+  constructor() {
+    this.api = new Api(`stories`);
+    this.api.getAll().then(d => d.forEach(this._addStory));
+  }
 
-	addStory = story => {
-		const newStory = new Story(story.title);
-		this.stories.push(newStory);
-		this.api
-			.create(newStory)
-			.then(storyValues => newStory.updateFromServer(storyValues));
-	};
+  addStory = story => {
+    const newStory = new Story(story.title);
+    this.stories.push(newStory);
+    this.api
+      .create(newStory)
+      .then(storyValues => newStory.updateFromServer(storyValues));
+  };
 
-	_addStory = ({ title, _id }) => {
-		const story = new Story(title, _id);
-		runInAction(() => this.stories.push(story));
-	};
+  _addStory = ({ title, _id }) => {
+    const story = new Story(title, _id);
+    runInAction(() => this.stories.push(story));
+  };
 }
 
 decorate(Store, {
-	stories: observable,
-	addStory: action
+  stories: observable,
+  addStory: action
 });
 
 export default new Store();
