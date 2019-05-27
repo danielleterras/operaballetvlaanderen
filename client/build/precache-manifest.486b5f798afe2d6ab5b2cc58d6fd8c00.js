f5b00d6de2f41bbbe2b3da811a02ca3d7a94d31c
sarahuyghe <sara.huyghe@student.howest.be>
2019-05-27 11:18:14 +0200

    adding proxy

---
+++ client/build/precache-manifest.486b5f798afe2d6ab5b2cc58d6fd8c00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2c4060bb8f62f6a35b37",
+    "url": "/static/js/main.29ac9ed5.chunk.js"
+  },
+  {
+    "revision": "93dd293e0a2159215c41",
+    "url": "/static/js/2.23d1285f.chunk.js"
+  },
+  {
+    "revision": "2c4060bb8f62f6a35b37",
+    "url": "/static/css/main.fc5cdb52.chunk.css"
+  },
+  {
+    "revision": "2390499d56a735d59064f4357073961c",
+    "url": "/index.html"
+  }
+];
