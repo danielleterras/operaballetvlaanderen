14224cba6efe03264c7117f4e8edd45cc4bdc0a9
Daniëlle Terras <danielle.terras@student.howest.be>
2019-06-04 18:44:05 +0200

    Update layout en assets

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -8,6 +8,14 @@ import Cube from "./Cube";
 import "./styles.css";
 import styles from "./home.module.css";
 import mouse from "./../assets/scroll_icon.svg";
+import cube1 from "./../assets/cube_1.svg";
+import cube2 from "./../assets/cube_2.svg";
+import cube3 from "./../assets/cube_3.svg";
+import cube4 from "./../assets/cube_4.svg";
+import cube5 from "./../assets/cube_5.svg";
+
+import cube_small from "./../assets/cube_small.svg";
+
 class FullpageWrapper extends Component {
   render() {
     return (
@@ -38,100 +46,253 @@ class FullpageWrapper extends Component {
                 </div>
               </div>
               <div className="section">
-                <h1>
-                  Ontmoet <b>Cubo</b>
+                <h1
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    flexDirection: "column"
+                  }}
+                >
+                  <span style={{ fontSize: "24px" }}>Ontdek</span>{" "}
+                  <span style={{ fontWeight: "bold" }}>Cubo</span>
                 </h1>
-                <div className={styles.cube}>{/*<Cube />*/}</div>
-                <p>Ervaar op een unieke manier</p>
-                <button className={styles.button}>Lees alle verhalen</button>
-              </div>
-              <div className="section">
-                <div className="testing">
+                <div
+                  className={styles.cube1}
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "650px"
+                  }}
+                >
                   {/*<Cube />*/}
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-                    molestiae corporis libero quisquam ut amet quia ea
-                    laudantium saepe minus unde ipsum culpa temporibus et quam
-                    numquam quo, nobis fuga.
-                  </p>
-                  <button className={styles.button}>
-                    Ontdek andere verhalen
-                  </button>
-                  <button className={styles.button}>Deel je verhaal</button>
+                  <img src={cube1} alt="cube1" width="300px" height="300px" />
+                </div>
+                <p style={{ display: "flex", justifyContent: "center" }}>
+                  Ervaar op een unieke manier
+                </p>
+              </div>
+              <div className="section">
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-around",
+                    alignItems: "center"
+                  }}
+                >
+                  <div
+                    className={styles.cube2}
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      height: "650px"
+                    }}
+                  >
+                    {/*<Cube />*/}
+                    <img src={cube2} alt="cube2" width="650px" height="350px" />
+                  </div>
+                  <div style={{ width: "600px", marginLeft: "-10rem" }}>
+                    <button aria-label="Ontdek" className={styles.sub}>
+                      Ontdek alle verhalen
+                    </button>
+                    <h2>
+                      Wil jij opera en ballet op een unieke manier te ervaren?
+                    </h2>
+                    <p>
+                      Dankzij <span style={{ fontWeight: "bold" }}>Cubo</span>{" "}
+                      worden niet alleen jouw ogen en oren geprikkeld maar word
+                      je helemaal ondergedompelt in de wereld van Opera Ballet
+                      Vlaanderen.
+                    </p>
+                    <p>
+                      Cubo reageert op wat zich voor jouw ogen afspeelt tijdens
+                      de voorstelling, door veranderingen in{" "}
+                      <span style={{ fontWeight: "bold" }}>kleur</span> en{" "}
+                      <span style={{ fontWeight: "bold" }}>trillingen</span>.
+                      Bij aanvang van de voorstelling geeft hij een korte{" "}
+                      introductie over de voorstelling die je gaat bijwonen.
+                      Hierna neem je Cubo mee in de zaal en ervaar je de show
+                      met een{" "}
+                      <span style={{ fontWeight: "bold" }}>extra zintuig</span>{" "}
+                      in jouw handen. Na de show kan je feedback of jouw verhaal
+                      meedelen aan Cubo.
+                    </p>
+                    <p>
+                      Eén van deze verhalen zal worden omgezet tot een korte
+                      ballet of opera voorstelling.{" "}
+                      <span style={{ fontWeight: "bold" }}>
+                        Deel jouw verhaal
+                      </span>{" "}
+                      en laat je horen, net zoals Opera Ballet Vlaanderen.
+                    </p>
+
+                    <button aria-label="Creëer" className={styles.button}>
+                      Creëer je verhaal
+                    </button>
+                    <button aria-label="Schrijf" className={styles.button}>
+                      Schrijf je verhaal
+                    </button>
+                  </div>
                 </div>
               </div>
               <div className="section">
-                {/*<Cube />*/}
-                <div>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod
-                  </p>
-                  <h2>Wat is de titel van je verhaal?</h2>
-                  <input
-                    type="text"
-                    placeholder="Typ hier je antwoord"
-                    className={styles.inputField}
-                  />
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-around",
+                    alignItems: "center"
+                  }}
+                >
+                  <div
+                    className={styles.cube3}
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      height: "650px"
+                    }}
+                  >
+                    {/*<Cube />*/}
+                    <img src={cube3} alt="cube3" width="650px" height="350px" />
+                  </div>
+                  <div style={{ width: "600px", marginLeft: "-10rem" }}>
+                    <p>
+                      We willen graag weten wie de auteur is van dit verhaal.
+                    </p>
+                    <h2>Wat is jouw naam?</h2>
+                    <input
+                      type="text"
+                      placeholder="Typ hier je antwoord"
+                      className={styles.inputField}
+                    />
+                  </div>
                 </div>
               </div>
               <div className="section">
-                {/*<Cube />*/}
-                <div>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod
-                  </p>
-                  <h2>Wat is jou naam?</h2>
-                  <input
-                    type="text"
-                    placeholder="Typ hier je antwoord"
-                    className={styles.inputField}
-                  />
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-around",
+                    alignItems: "center"
+                  }}
+                >
+                  <div
+                    className={styles.cube4}
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      height: "650px"
+                    }}
+                  >
+                    {/*<Cube />*/}
+                    <img src={cube4} alt="cube4" width="650px" height="350px" />
+                  </div>
+                  <div style={{ width: "600px", marginLeft: "-10rem" }}>
+                    <p>Kies een personage dat jou aanspreekt.</p>
+                    <h2>Wie zou je willen zijn?*</h2>
+                    <input
+                      type="text"
+                      placeholder="Typ hier je antwoord"
+                      className={styles.inputField}
+                    />
+                    <p>*Koningin, weduwe, prins detective, wetenschapper,...</p>
+                  </div>
                 </div>
               </div>
               <div className="section">
-                {/*<Cube />*/}
-                <div>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod
-                  </p>
-                  <h2>Wie zou je willen zijn?</h2>
-                  <input
-                    type="text"
-                    placeholder="Typ hier je antwoord"
-                    className={styles.inputField}
-                  />
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-around",
+                    alignItems: "center"
+                  }}
+                >
+                  <div
+                    className={styles.cube5}
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      height: "650px"
+                    }}
+                  >
+                    {/*<Cube />*/}
+                    <img src={cube5} alt="cube5" width="650px" height="350px" />
+                  </div>
+                  <div style={{ width: "600px", marginLeft: "-10rem" }}>
+                    <p>Omschrijf je thema.</p>
+                    <h2>Welke stijl verkies je?</h2>
+                    <input
+                      type="text"
+                      placeholder="Typ hier je antwoord"
+                      className={styles.inputField}
+                    />
+                  </div>
                 </div>
               </div>
               <div className="section">
-                {/*<Cube />*/}
-                <div>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod
-                  </p>
-                  <h2>Welke stijl verkies je?</h2>
-                  <input
-                    type="text"
-                    placeholder="Typ hier je antwoord"
-                    className={styles.inputField}
-                  />
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-around",
+                    alignItems: "center"
+                  }}
+                >
+                  <div
+                    className={styles.cube6}
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      height: "650px"
+                    }}
+                  >
+                    {/*<Cube />*/}
+                    <img src={cube1} alt="cube2" width="650px" height="350px" />
+                  </div>
+                  <div style={{ width: "600px", marginLeft: "-10rem" }}>
+                    <p>Omschrijf je verhaal in één zin.</p>
+                    <h2>Wat is de titel van je verhaal?</h2>
+                    <input
+                      type="text"
+                      placeholder="Typ hier je antwoord"
+                      className={styles.inputField}
+                    />
+                  </div>
                 </div>
               </div>
               <div className="section">
-                {/*<Cube />*/}
-                <div>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod
-                  </p>
-                  <h2>Vertel een leuke anekdote over jezelf.</h2>
-                  <textarea
-                    placeholder="Typ hier je antwoord"
-                    className={styles.textField}
-                  />
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-around",
+                    alignItems: "center"
+                  }}
+                >
+                  <div>
+                    {/*<Cube />*/}
+                    <img
+                      src={cube_small}
+                      alt="cube2"
+                      width="650px"
+                      height="350px"
+                    />
+                  </div>
+                  <div>
+                    <p>
+                      Om het verhaal een persoonlijke twist te geven, kan je
+                      hier iets schrijven dat jou is overkomen in het thema dat
+                      je hebt gekozen (stap 06).
+                    </p>
+                    <h2>Vertel een leuke anekdote over jezelf.</h2>
+                    <textarea
+                      placeholder="Typ hier je antwoord"
+                      className={styles.textField}
+                    />
+                  </div>
                 </div>
               </div>
               <div className="section">
@@ -156,10 +317,8 @@ class FullpageWrapper extends Component {
                     distinctio nostrum ratione itaque rerum. Consequatur,
                     dolore.
                   </p>
-                  <button className={styles.submit}>Deel je verhaal</button>
-                  <button className={styles.button}>
-                    Ontdek andere verhalen
-                  </button>
+                  <button className={styles.button}>Deel je verhaal</button>
+                  <button className={styles.sub}>Ontdek alle verhalen</button>
                 </div>
               </div>
               <div className="section">
